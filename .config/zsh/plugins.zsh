da2b904a25da0293d957d3ec863f370b11798e1f
Hirotaka Sugawara <sugawara@fun.bio.keio.ac.jp>
2026-08-09 19:26:26 +0900

    refactor: migrate zsh plugins to sheldon

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,12 +1,3 @@
 if (( $+commands[sheldon] )); then
   eval "$(sheldon source)"
-else
-  # Sheldon導入前やネットワーク障害時の移行用フォールバック。
-  if [[ -r "$DOTFILES_ROOT/zsh-autosuggestions/zsh-autosuggestions.zsh" ]]; then
-    builtin source "$DOTFILES_ROOT/zsh-autosuggestions/zsh-autosuggestions.zsh"
-  fi
-
-  if [[ -r "$DOTFILES_ROOT/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
-    builtin source "$DOTFILES_ROOT/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-  fi
 fi
